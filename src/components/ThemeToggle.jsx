4dd4fda2a9c791fc0c161bdfe45510d75332e39a
sinentantiso <103891865+sinentantiso@users.noreply.github.com>
2022-12-13 22:43:56 +0200

    1 file modified

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { HiSun, HiMoon } from 'react-icons/ai';
 import ThemeContext from './context/ThemeContext';
 
 const ThemeToggle = () => {
-  return (
+  const {theme, setTheme} = useContext(ThemeContext)
 
+
+  return (
+    <div>
+      {theme === 'dark' ? (
+        <div></div>
+      ): <div></div>}
+    </div>
   )
 }
 
