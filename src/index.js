80ddcc0198085c319fab3e35cd7a37001d192688
sinentantiso <103891865+sinentantiso@users.noreply.github.com>
2022-12-13 22:10:13 +0200

    1 file modified

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
 
 import App from './App';
 
@@ -7,7 +9,7 @@ const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
-  <StrictMode>
+  <BrowserRouter>
     <App />
-  </StrictMode>
+  </BrowserRouter>
 );
