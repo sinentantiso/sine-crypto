a75a41b4f095d6bf93109204c5926ac377f51107
sinentantiso <103891865+sinentantiso@users.noreply.github.com>
2022-12-14 21:53:21 +0200

    1 file added

---
+++ src/tailwind.config.js
@@ -0,0 +1,23 @@
+Module.export = {
+  content: ['./src/**/*/.{js,jsx,ts,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      backgroundColor: {
+        primary: 'var(--color-bg-primary)',
+        secondary: 'var(--color-bg-secondary)',
+        button: 'var(--color-bg-button)',
+      },
+      TextColor: {
+        Accent: 'var(--color-text-primary)',
+        primary: 'var(--color-text-primary)',
+        secondary: 'var(--color-text-secondary)',
+        btnText: 'var(--color-bg-secondary)',
+      },
+      borderColor: {
+        
+      }
+    }
+  },
+  plugins: [],
+};
