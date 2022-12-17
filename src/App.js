98a5ba2590565d64b12a84fde1c853313a3852e9
Sinethemba Ntantiso <sinethemberntantiso@gmail.com>
2022-12-17 16:37:51 +0200

    update

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React from "react";
 import "./index.css";
 import Navbar from "./components/Navbar";
 import { ThemeProvider } from "./context/ThemeContext";
+import axios from "axios"
+
+import Account from "./routes/Account";
+import Home from "./routes/Home";
+import Signin from "./routes/Signin";
+import Signup from "./routes/Signup";
+import { Route, Routes } from "react-router-dom";
 
 
 export default function App() {
   return (
     <ThemeProvider>
       <Navbar />
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/signin" element={<Signin />}/>
+        <Route path="/signup" element={<Signup />}/>
+        <Route path="/account" element={<Account />}/>
+      </Routes>
     </ThemeProvider>
   );
 }
