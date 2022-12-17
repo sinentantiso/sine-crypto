44dae73e550b831c9c004e9f0d152735c2b966f4
sinentantiso <103891865+sinentantiso@users.noreply.github.com>
2022-12-17 20:44:55 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./index.css";
-import { ThemeProvider } from "./context/ThemeContext";
+
 import Navbar from "./components/Navbar";
+
 
 
 export default function App() {
   return (
     <ThemeProvider>
+      <Navbar />
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
       
